cdb4c65071e64f0ec81f958b52d4365fe6367833
slyjay <duahsylvester24@gmail.com>
2024-01-01 20:20:49 -0600

    urls.py

--- extend/home/urls.py
+++ extend/home/urls.py
@@ -1,21 +1,21 @@
+# Import modules/packages
 from django.urls import path
 from .views import ExtendListView, OverdriveView, HormoneView, HomeRedirectView
 
+# Define the URL patterns for the app
 urlpatterns = [
-    path('', HomeRedirectView.as_view(), name='home-redirect'),  # Redirect root URL to extend-list
+    # Root URL redirects to 'extend-list'
+    path('', HomeRedirectView.as_view(), name='home-redirect'),
+
+    # URL for ExtendListView, mapped to 'extend-list'
     path('extend/', ExtendListView.as_view(), name='extend-list'),
 
-    
-
-    # Overdrive - PowerWall
+    # URLs for OverdriveView (PowerWall)
     path('overdrive/', OverdriveView.as_view(), name='overdrive-list'),
     path('overdrive/<int:pk>/', OverdriveView.as_view(), name='overdrive-detail'),
     
-    
-    # Hormone - Ecommerce
+    # URLs for HormoneView (Ecommerce)
     path('hormones/', HormoneView.as_view(), name='hormones-list'),
     path('hormones/<int:pk>/', HormoneView.as_view(), name='hormones-detail'),
     path('hormones/<int:pk>/producer-dashboard/', HormoneView.as_view(), name='producer-dashboard'),
 ]
-
-
