96714d470445fc37e39a6d8e2b339d357a0aff3e
slyjay <duahsylvester24@gmail.com>
2024-01-01 20:14:49 -0600

    admin.py

--- extend/home/admin.py
+++ extend/home/admin.py
@@ -1,6 +1,19 @@
+# Import modules/packages
 from django.contrib import admin
 from .models import Extend, Overdrive, Hormone
 
+
+
+
+
+
+# Manage and view 'Extend' objects through the admin interface.
 admin.site.register(Extend)
+
+# Manage and view 'Overdrive' objects through the admin interface.
 admin.site.register(Overdrive)
+
+# Manage and view 'Hormone' objects through the admin interface.
 admin.site.register(Hormone)
+
+
